b43eaacce51411d175aa123cf8e8c429bbd55e79
Khloe-joanie <khloejoanne@gmail.com>
2021-08-19 16:50:17 +0300

    Changes made to title

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 /** @format */
 
 import React, { useState } from "react";
-import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
 import { Link } from "react-router-dom";
 import SidebarData from "./SidebarData";
@@ -14,7 +13,6 @@ function Navbar() {
   return (
     <>
       <Link onClick={showSidebar} to="#" className="menu-bars">
-        {/* <FaIcons.FaBars /> */}
         <div className="menu-icon">
           <div className="bar"></div>
           <div className="bar"></div>
